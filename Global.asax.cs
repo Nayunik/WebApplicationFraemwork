6a31d3fd8f3feeab2c7b120a9dccf126ad1a9bcf
Nayunik <koryukin.danil@list.ru>
2022-03-10 23:21:48 +0500

    В модель добавил инициализатор БД

--- Global.asax.cs
+++ Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data.Entity;
+using WebApplicationFraemwork.Models;
 
 namespace WebApplicationFraemwork
 {
@@ -12,6 +14,8 @@ namespace WebApplicationFraemwork
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new BookDbInitializer());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
