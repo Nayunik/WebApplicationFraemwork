6b07bbb60ce7254ace85011ebcc4be9734b13e7c
Nayunik <koryukin.danil@list.ru>
2022-03-10 22:38:17 +0500

    Добавлена модель покупок Purchase

---
+++ Models/Purchase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationFraemwork.Models
+{
+    public class Purchase
+    {
+        //Id покупки
+        public int PurchaseId { get; set; }
+        //ФИО покупателя
+        public string Person { get; set; }
+        //Адрес покупателя
+        public string Address { get; set; }
+        //Id книги
+        public int BookId { get; set; }
+        //Дата и время покупки
+        public DateTime Date { get; set; }
+    }
+}
